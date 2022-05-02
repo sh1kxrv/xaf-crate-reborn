a271195ef2414b2a7b44627519036e79c17959c5
Егор <ItsAwware@yandex.ru>
2022-05-02 09:07:14 +0600

    build: file utils is done

--- src/utils/__test__/file.test.ts
+++ src/utils/__test__/file.test.ts
@@ -1,5 +1,34 @@
-describe('Комплекс тестов модуля File', () => {
-  test('Тест: рекурсивное копирование', () => {
-    expect(2 + 2).toBe(4)
+import * as fs from 'fs'
+import * as path from 'path'
+import { copy, deep_readdir } from '../file'
+
+const validate_directory = (src: string[], dest: string[]): boolean => {
+  return (
+    src.length === dest.length &&
+    src.every((path, index) => dest[index] === path)
+  )
+}
+
+const makeRelative = (base: string) => {
+  return (p) => path.relative(base, p)
+}
+
+describe('Комплекс тестов модуля utils/file', () => {
+  const test_field = path.resolve(__dirname, './test-field')
+  const test_field_copy = path.resolve(__dirname, './test-field-copy')
+
+  test('Тест: рекурсивное копирование директории', () => {
+    copy(test_field, test_field_copy)
+
+    const src = [...deep_readdir(test_field)].map(makeRelative(test_field))
+    const dest = [...deep_readdir(test_field_copy)].map(
+      makeRelative(test_field_copy)
+    )
+
+    expect(validate_directory(src, dest)).toBeTruthy()
+  })
+
+  afterAll(() => {
+    fs.rmdirSync(test_field_copy, { recursive: true })
   })
 })
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,21 +1,38 @@
-import fs from 'fs'
-import path from 'path'
+import * as fs from 'fs'
+import * as path from 'path'
 
 /**
+ * Копирование файлов из директории A в директорию B
+ * Поддерживает рекурсивное копирование
  * @param {string} src
  * @param {string} target
  */
-export function copy(src, target) {
+export function copy(src: string, target: string) {
   const files = fs.readdirSync(src)
   for (const file_path of files) {
-    const stat = fs.lstatSync(file_path)
-    const relative = path.relative(src, file_path)
-    if (stat.isDirectory()) {
-      const new_directory = path.resolve(target, relative)
-      fs.mkdirSync(new_directory)
-      copy(target, new_directory)
+    const full_path = path.resolve(src, file_path)
+    const new_target_path = path.resolve(target, file_path)
+    const stats = fs.statSync(full_path)
+    if (stats.isDirectory()) {
+      if (!fs.existsSync(new_target_path))
+        fs.mkdirSync(new_target_path, { recursive: true })
+      copy(full_path, new_target_path)
     } else {
-      fs.copyFileSync(target, relative)
+      fs.copyFileSync(full_path, new_target_path)
     }
+  }
+}
+
+/**
+ * Производит глубокое ( рекурсивное ) чтение путей из директории
+ * @param {string} target
+ */
+export function* deep_readdir(target: string): Generator<string> {
+  const files = fs.readdirSync(target)
+  for (const file_name of files) {
+    const full_path = path.resolve(target, file_name)
+    const stats = fs.statSync(full_path)
+    if (stats.isDirectory()) yield* deep_readdir(full_path)
+    yield full_path
   }
 }
