662018dd9c42fd1040db1c606c23c61bcbe102a4
Егор <ItsAwware@yandex.ru>
2022-08-23 15:35:01 +0600

    chore: eslint + prettier setting's up

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['jest', '@typescript-eslint'],
+  env: {
+    'jest/globals': true,
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+}
