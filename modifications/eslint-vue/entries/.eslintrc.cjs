6eb95522a5d836c13020fbdb945c83801427a353
Егор <ItsAwware@yandex.ru>
2022-08-23 12:59:06 +0600

    build(eslint-mod): jest support

--- modifications/eslint-vue/entries/.eslintrc.cjs
+++ modifications/eslint-vue/entries/.eslintrc.cjs
@@ -4,11 +4,13 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  plugins: ['jest'],
   env: {
     'vue/setup-compiler-macros': true,
     browser: true,
     amd: true,
     node: true,
+    'jest/globals': true,
   },
   parserOptions: {
     ecmaVersion: 13,
@@ -19,6 +21,12 @@ module.exports = {
       {
         endOfLine: 'auto',
         printWidth: 120,
+      },
+    ],
+    'vue/multi-word-component-names': [
+      'off',
+      {
+        ignores: [],
       },
     ],
   },
