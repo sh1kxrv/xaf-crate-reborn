243bfb1a2c2ef8cd2c3910d7154ec17ebcb0e5c5
Егор <ItsAwware@yandex.ru>
2022-08-24 21:01:47 +0600

    chore: logger types update

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,26 +1,26 @@
-import { bold, lightGreen, yellow, bgLightRed, bgRed, lightRed } from 'kolorist'
+import { bold, lightGreen, yellow, lightRed, green, white } from 'kolorist'
 import { version } from '../../package.json'
 
-export function log(message: string, prefix = '📦', ...args: any[]) {
+export function log(message: string, prefix = '📦', ...args: unknown[]) {
   console.log(`[${prefix}] ${bold(message)}`, ...args)
 }
 
-export function successfully(message) {
+export function successfully(message: string) {
   log(`${lightGreen(message)} 🪄`, '⭐️')
 }
 
-export function angry(msg) {
+export function angry(msg: string) {
   log(`${lightRed(msg)}`, '👺')
 }
 
-export function info(msg) {
+export function info(msg: string) {
   log(msg, '🐌')
 }
 
-export function warn(msg) {
+export function warn(msg: string) {
   log(`${yellow(msg)}`, '🪐')
 }
 
 export function hello() {
-  log(bgRed(` xaf-crate ${bgLightRed('REBORN')} | v${version}`))
+  log(green(`(${white('REBORN')}) xaf-crate | ${white(`v${version}`)}`))
 }
