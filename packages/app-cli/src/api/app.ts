063de20e5b5af5874e9f411397a228eb9f70b0ab
xafxafxaf <ItsAwware@yandex.ru>
2022-08-29 06:11:44 +0600

    build: major update
    - moved `templates` & `modification` -> `packages/assets`
    - app bootstrap
    - `@xfcr/shared` crate

--- packages/app-cli/src/api/app.ts
+++
@@ -1,3 +0,0 @@
-export interface App {
-  bootstrap(): Promise<void>
-}
--- packages/app-cli/src/app.ts
+++ packages/app-cli/src/app.ts
@@ -1,6 +1,25 @@
-import { Storage } from '@xfcr/shared'
+import Yargs from 'yargs'
+
+async function parse_cli() {
+  const yargs = Yargs(process.argv.slice(2))
+  const argv = await yargs.argv
+  function get<T>(key: string): T | null {
+    return argv[key] as T | null
+  }
+  return {
+    get,
+    argv,
+  }
+}
 class App {
-  bootstrap() {}
+  async bootstrap() {
+    const { get } = await parse_cli()
+    const crate: string | null = get('mode')
+    console.log(crate ?? 'NULL')
+  }
 }
 
-new App().bootstrap()
+;(async () => {
+  const app = new App()
+  await app.bootstrap()
+})().catch(console.error)
