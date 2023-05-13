3bffbc9039e2c63d0cac4cc8ff2835a18bda85ad
xafxafxaf <ItsAwware@yandex.ru>
2023-05-13 20:12:44 +0600

    chore: useless mods

--- modifications/express-ts-sequelize/entries/src/packages/module/module.ts
+++
@@ -1,9 +0,0 @@
-export default class Module {
-  name: string
-
-  constructor(name: string) {
-    this.name = name
-  }
-
-  register(...args) {}
-}
--- modifications/express-ts-sequelize/entries/src/routes/route.ts
+++
@@ -1,5 +0,0 @@
-import type { Router } from 'express'
-export interface Route {
-  path: string
-  router: Router
-}
